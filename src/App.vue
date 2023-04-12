4a67dfb2946b6444945a7c0a0661972a67f14781
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-04-12 17:02:00 +0200

    added AppTitle

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  
+  <AppTitle />
 </template>
 
 <script>
-
+  import AppTitle from './components/AppTitle.vue';
+  export default {
+    name: 'App',
+    components: {
+      AppTitle,
+    }
+  };
 </script>
 
 <style lang="css" scoped>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ titolo }}</h1>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',
+        data() {
+            return {
+            titolo: 'La mia prima app con Vite!',
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+  
+</style>
